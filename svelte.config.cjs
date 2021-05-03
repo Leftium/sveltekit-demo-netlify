49f1ff9131552ee90d463c06aee8dc9a6ce9f101
Leftium <john@leftium.com>
2021-05-04 01:41:11 +0900

    Add netlify adapter.

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,6 +1,9 @@
+netlify =  require('@sveltejs/adapter-netlify')
+
 /** @type {import('@sveltejs/kit').Config} */
 module.exports = {
 	kit: {
+		adapter: netlify(),
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	}
